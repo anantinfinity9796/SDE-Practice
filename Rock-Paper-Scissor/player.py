70233474f76b4c573d588f587f15899a4e86c211
anantinfinity9796 <56185895+anantinfinity9796@users.noreply.github.com>
2024-01-10 22:58:01 +0530

    added test cases for player class

--- Rock-Paper-Scissor/player.py
+++ Rock-Paper-Scissor/player.py
@@ -4,20 +4,25 @@
 class Player():
     """ Class defining the player and its methods to play the game"""
 
-    def __init__(self, name:str='default_player', score:int=0, moves:list=None ) -> None:
+    def __init__(self, name:str='default_player', score:int=0, moves:list=[] ) -> None:
         self._name=name
         self._score=score
         self._moves=moves
 
     @property
     def score(self):
-        """ Get the current score of the player"""
+        """ Getter Method for score attribute"""
         return self._score
     
     @property
     def name(self):
-        """ Getter method for the name property"""
+        """ Getter method for the name attribute"""
         return self._name
+    
+    @property
+    def moves(self):
+        """ Getter method for moves list"""
+        return self._moves
     
     def increase_score(self):
         self._score+=1
--- Rock-Paper-Scissor/tests/test_player.py
+++ Rock-Paper-Scissor/tests/test_player.py
@@ -1,4 +1,4 @@
-from ..player import Player
+from player import Player
 import pytest
 
 @pytest.fixture
@@ -14,4 +14,12 @@ def create_player_without_name():
 
 def test_player_name_getter(create_player):
     assert create_player.name == "dummy_player_1"
-    assert create_player.name == 'dummy_player_1'
+    assert create_player.score == 0
+    create_player.increase_score()
+    assert create_player.score == 1
+    assert create_player.moves == []
+
+def test_player_name_with_default(create_player_without_name):
+    assert create_player_without_name.name == "default_player"
+    assert create_player_without_name.score == 0
+    assert create_player_without_name.moves == []
