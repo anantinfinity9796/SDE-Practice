21c865a46a391e17b839e8eccb6d78fce9deb6d5
anantinfinity9796 <56185895+anantinfinity9796@users.noreply.github.com>
2024-01-20 21:49:46 +0530

    Added the ecoding logic | addition and retrieval of collection metadata

--- toy_database/classes/collection_metadata.py
+++ toy_database/classes/collection_metadata.py
@@ -4,27 +4,27 @@ from interfaces.icollection_metadata import ICollectionMetadata
 
 
 class CollectionMetadata(ICollectionMetadata):
-    """ Class for defining the metadata of the collections"""
+    """ 1. Class for defining the metadata of the collections.
+        2. We will have dytpe:filename mapping and we will have adding methods to add dtypes.
+    """
 
-    def __init__(self, collection_type:str, ):
-        # self._metadata_dict = {}
-        self._collection_type = collection_type
-        self._key_valueoffset_dict = {}
+    def __init__(self):
+        self._metadata_dict = {}
 
-        # We will define the filename based on the data_type | Data repr : <<key_len><key>:string<valuelen><value>:collection_type>>
-        self._filename = f"collection_{collection_type}.bat"
-
+    def read_collection_metadata(self, dtype:str):
+        return self._metadata_dict[dtype]
     
-    @property
-    def collection_type(self):
-        return self._collection_type
-
-
-    def read_metadata(self, dtype:str):
-        if dtype =='int':
-            
-        pass
-    
-    def write_metadata(self, ):
+    def write_new_collection_metadata(self, dtype:str):
         """ This function will write the metadata into the dict"""
-        pass
+        if dtype in self._metadata_dict.keys():
+            print("Dtype already present. No need to add")
+
+        else:
+            # We will define the filename based on the data_type | Data repr : <<key_len><key>:string<valuelen><value>:collection_type>>
+            if dtype in ['int','str','float','bool']:
+                filename = f"collection_{dtype}.bat"
+                self._metadata_dict[dtype] = filename
+            else:
+                raise ValueError("Provided dtype not supported. | Supported dtypes are ['int','str','float','bool']")
+            
+        return
--- toy_database/interfaces/idatatype.py
+++ toy_database/interfaces/idatatype.py
@@ -1,11 +1,13 @@
 # idatatype.py
 
 class IDatatype():
-    def __init__(self):
+    def __init__(self, dtype:str):
         pass
 
-    def encode(self):
+    def encode(self) -> bytes:
+        """This function will encode the value passed in it based on the datatype"""
         pass
 
-    def decode(self):
+    def decode(self, value:bytes) -> str:
+        """ This function will decode the bytes passed in according to the dtype and convert it to a str"""
         pass
